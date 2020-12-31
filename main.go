e02827a5348240f968b239a84191a8e4862b5c44
Marcus R. Breese <marcus@breese.com>
2020-12-31 04:02:32 -0500

    cleanup

--- main.go
+++ main.go
@@ -5,25 +5,5 @@ import (
 )
 
 func main() {
-	// if os.Args[1] == "server" {
-	// 	args := make([]string, len(os.Args)-1)
-	// 	forkme := false
-	// 	for i, arg := range os.Args[1:] {
-	// 		if arg == "-d" || arg == "--daemon" {
-	// 			forkme = true
-	// 		} else if forkme {
-	// 			args[i-1] = arg
-	// 		} else {
-	// 			args[i] = arg
-	// 		}
-	// 	}
-	// 	if forkme {
-	// 		pid, _ := Fork(args, "test.out")
-	// 		fmt.Printf("%d\n", pid)
-	// 		os.Exit(0)
-	// 	}
-
-	// }
-
 	cmd.Execute()
 }
