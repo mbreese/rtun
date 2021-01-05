e438ad7250c6f9161782db5983d3ddf62a4ea2c9
Marcus R. Breese <marcus@breese.com>
2021-01-05 02:08:35 +0000

    notify updates to allow empty messages

--- server/oshandler/linux.go
+++ server/oshandler/linux.go
@@ -9,7 +9,19 @@ import (
 
 // Notify send a desktop notification
 func Notify(msg string, title string) {
-	cmd2 := exec.Command("notify-send", title, msg)
+
+	var cmd2 *exec.Cmd
+	if msg != "" && title != "" {
+		cmd2 = exec.Command("notify-send", title, msg)
+	} else if msg == "" && title != "" {
+		cmd2 = exec.Command("notify-send", title)
+	} else if msg != "" && title == "" {
+		cmd2 = exec.Command("notify-send", msg)
+	} else {
+		cmd2 = exec.Command("notify-send")
+	}
+
+	// cmd2 := exec.Command("notify-send", title, msg)
 	err2 := cmd2.Run()
 	if err2 != nil {
 		fmt.Printf("Got err? %v (Notify on Linux requires \"notify-send\")\n", err2)
--- server/oshandler/mac.go
+++ server/oshandler/mac.go
@@ -9,7 +9,16 @@ import (
 
 // Notify send a desktop notification
 func Notify(msg string, title string) {
-	cmd2 := exec.Command("osascript", "-e", fmt.Sprintf("display notification \"%s\" with title \"%s\"", msg, title))
+	if msg != "" && title != "" {
+		cmd2 = exec.Command("osascript", "-e", fmt.Sprintf("display notification \"%s\" with title \"%s\"", msg, title))
+	} else if msg == "" && title != "" {
+		cmd2 = exec.Command("osascript", "-e", fmt.Sprintf("display notification \"%s\"", title))
+	} else if msg != "" && title == "" {
+		cmd2 = exec.Command("osascript", "-e", fmt.Sprintf("display notification \"%s\"", msg))
+	} else {
+		cmd2 = exec.Command("osascript", "-e", fmt.Sprintf("display notification"))
+	}
+	// cmd2 := exec.Command("osascript", "-e", fmt.Sprintf("display notification \"%s\" with title \"%s\"", msg, title))
 	err2 := cmd2.Run()
 	if err2 != nil {
 		fmt.Printf("Got err? %v\n", err2)
